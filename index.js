132b906e3e708bdf5aebe3358ef8e5fc72051bbb
Angelo Geulin <angelogeulin123@gmail.com>
2023-03-15 13:44:45 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const cors = require("cors")
+const app = express()
+
+app.use(cors())
+
+app.get("/movie/search/:searchTerm", async (req, res) => {
+  const apiKey = process.env.TMDB_API_KEY
+  const link = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${req.params.searchTerm}&page=1&include_adult=false`
+  const response = await fetch(link)
+  const data = await response.json()
+
+  res.json({
+    message: `Search results for: ${req.params.searchTerm}`,
+    result: data.results,
+  })
+})
+
+const port = process.env.PORT | 3000
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port} ...`)
+})
