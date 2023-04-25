ed018bee2a80b2b731ab4a5f94f37a0d93bbdd9e
Angelo Geulin <angelogeulin123@gmail.com>
2023-04-25 16:33:57 +0800

    refactor: use tmdb-ts to retrieve movies

--- index.ts
+++ index.ts
@@ -1,86 +1,77 @@
 import { config } from "dotenv"
 import express from "express"
 import cors from "cors"
+import TMDB from "tmdb-ts"
 
 config()
-const { TMDB_API_KEY } = process.env
-if (typeof TMDB_API_KEY !== "string") throw new Error("Invalid TMDB API key")
+const { TMDB_ACCESS_TOKEN } = process.env
+if (typeof TMDB_ACCESS_TOKEN !== "string")
+  throw new Error("Invalid TMDB access token")
 
+const tmdb = new TMDB(TMDB_ACCESS_TOKEN)
 const app = express()
 app.use(cors())
 
 //now showing
 app.get("/movie/now_showing", async (req, res) => {
-  const link = `https://api.themoviedb.org/3/trending/movie/week?api_key=${TMDB_API_KEY}`
-  const response = await fetch(link)
-  const data = await response.json()
+  const { results } = await tmdb.trending.trending("movie", "week")
 
   res.json({
     message: `Search results for Now showing`,
-    result: data.results,
+    results,
   })
 })
 
 //Popular movie
 app.get("/movie/popular/movie", async (req, res) => {
-  const link = `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB_API_KEY}&language=en-US&page=1`
-  const response = await fetch(link)
-  const data = await response.json()
+  const { results } = await tmdb.movies.popular()
 
   res.json({
     message: `Search results for Popular Movie`,
-    result: data.results,
+    results,
   })
 })
 
 //Popular TV
 app.get("/movie/popular/tv", async (req, res) => {
-  const link = `https://api.themoviedb.org/3/tv/popular?api_key=${TMDB_API_KEY}&language=en-US&page=1`
-  const response = await fetch(link)
-  const data = await response.json()
+  const { results } = await tmdb.tvShows.popular()
 
   res.json({
     message: `Search results for Popular TV Shows`,
-    result: data.results,
+    results,
   })
 })
 
 //Popular MOVIE AND TV SHOWS
 app.get("/movie/popular", async (req, res) => {
-  const link = `https://api.themoviedb.org/3/tv/popular?api_key=${TMDB_API_KEY}&language=en-US&page=1`
-  const response = await fetch(link)
-  const data = await response.json()
-  const link2 = `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB_API_KEY}&language=en-US&page=1`
-  const response2 = await fetch(link2)
-  const data2 = await response2.json()
+  const popularMovies = await tmdb.movies.popular()
+  const popularTvShows = await tmdb.tvShows.popular()
 
   res.json({
     message: `Search results for Popular Movies And TV Shows`,
-    result: [...data.results, ...data2.results],
+    results: [...popularMovies.results, ...popularTvShows.results],
   })
 })
 
 //search movie
 app.get("/movie/search/:searchTerm", async (req, res) => {
-  const link = `https://api.themoviedb.org/3/search/movie?api_key=${TMDB_API_KEY}&language=en-US&query=${req.params.searchTerm}&page=1&include_adult=false`
-  const response = await fetch(link)
-  const data = await response.json()
+  const { results } = await tmdb.search.movies({
+    query: req.params.searchTerm,
+  })
 
   res.json({
     message: `Search results for: ${req.params.searchTerm}`,
-    result: data.results,
+    results,
   })
 })
 
 //search movie_id
 app.get("/movie/:movieid", async (req, res) => {
-  const link = `https://api.themoviedb.org/3/movie/${req.params.movieid}?api_key=${TMDB_API_KEY}&language=en-US`
-  const response = await fetch(link)
-  const data = await response.json()
+  const movie = await tmdb.movies.details(req.params.movieid)
 
   res.json({
     message: `Retrieved Movie with ID: ${req.params.movieid}`,
-    result: data,
+    result: movie,
   })
 })
 
