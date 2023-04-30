171bd092239e65bf15682661c6c05c804f17c3d6
Angelo Geulin <angelogeulin123@gmail.com>
2023-04-30 13:09:21 +0800

    feat: add routes for getting saved and reviewed movies

--- src/routers/user.ts
+++ src/routers/user.ts
@@ -2,6 +2,7 @@ import { Router, Request, Response } from "express"
 import { ZodError, z } from "zod"
 import { getAllUsers, getUserProfile } from "../services/firebase"
 import { UserRecord } from "firebase-admin/auth"
+import { prisma } from "../services/prisma"
 export const userRouter = Router()
 
 const searchSchema = z.object({
@@ -54,6 +55,76 @@ userRouter.get("/:userId", async (req: Request, res: Response) => {
     res.json({
       message: `Retrieved user profile`,
       result: userRecord as UserRecord,
+    })
+  } catch (e) {
+    if (e instanceof ZodError) {
+      res.status(400).json({
+        message: "Validation error occured",
+        error: e,
+      })
+      return
+    }
+
+    res.status(500).json({
+      message: "Unknown error occured",
+      error: e,
+    })
+  }
+})
+
+const getSavedSchema = z.object({
+  userId: z.string().length(28),
+})
+
+userRouter.get("/:userId/saved", async (req: Request, res: Response) => {
+  try {
+    const { userId } = getSavedSchema.parse({
+      userId: req.params.userId,
+    })
+    const userMovies = await prisma.userMovie.findMany({
+      where: {
+        userId,
+      },
+    })
+
+    res.json({
+      message: `Retrieved saved list`,
+      results: userMovies,
+    })
+  } catch (e) {
+    if (e instanceof ZodError) {
+      res.status(400).json({
+        message: "Validation error occured",
+        error: e,
+      })
+      return
+    }
+
+    res.status(500).json({
+      message: "Unknown error occured",
+      error: e,
+    })
+  }
+})
+
+const getReviewedSchema = z.object({
+  userId: z.string().length(28),
+})
+
+userRouter.get("/:userId/reviewed", async (req: Request, res: Response) => {
+  try {
+    const { userId } = getReviewedSchema.parse({
+      userId: req.params.userId,
+    })
+    const reviewedMovies = await prisma.review.findMany({
+      where: {
+        userId,
+      },
+    })
+
+    res.json({
+      message: `Retrieved reviewed movies list`,
+      results: reviewedMovies,
     })
   } catch (e) {
     if (e instanceof ZodError) {
